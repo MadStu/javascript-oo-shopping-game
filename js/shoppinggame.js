f1686b6fa3d8432f8e2c06c4762792c7a32d2bce
MadStu <madstu@gmail.com>
2022-08-18 17:37:08 +0100

    72

--- js/shoppinggame.js
+++ js/shoppinggame.js
@@ -363,6 +363,7 @@ function init(data) {
                 rateAndExit();
             } else {
                 // Call rating setter method of playerRating to set user entered rate value here
+                playerRating.rating = r;
                 
                 // Call Object.assign() method here to populate target
                 
